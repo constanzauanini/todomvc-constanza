a9f9fcedac09db4800b5dc276f5a0c89dce023ff
Constanza Uanini <constanza.uanini@globant.com>
2013-12-02 13:01:29 -0300

    I've been working on app.js and pendientes.view. I added others libraries and I modified the index.html so I can use underscore

--- src/js/collections/pendientes.collection.js
+++ src/js/collections/pendientes.collection.js
@@ -3,7 +3,6 @@ var app = app || {};
 app.Pendientes = Backbone.Collection.extend({
 	
 	 model: app.Pendiente,
-	 localStorage: new Backbone.LocalStorage('pendientes-backbone')
-	
+	 localStorage: new Backbone.LocalStorage('pendientes-backbone')	
 });
 
--- src/js/views/pendientes.view.js
+++ src/js/views/pendientes.view.js
@@ -1,6 +1,57 @@
 var app = app || {};
-
 app.AppView = Backbone.View.extend({
-	
-	 el: '#pendientesapp',
-}
+	el : '#pendientesapp',
+	initialize : function() {
+		this.$input = this.$('#new-todo');
+		this.$main = this.$('#main');
+		this.listenTo(app.Pendientes, 'add', this.addOne);
+	},
+	addOne : function(pendiente) {
+		var view = new app.PendienteView({
+			model : pendiente
+		});
+		$('#todo-list').append(view.render().el);
+	},
+});
+
+app.PendienteView = Backbone.View.extend({
+	tagName : 'li',
+	template: _.template( $('#item-template').html() ),
+	events : {
+		'dblclick label' : 'edit',
+		'keypress .edit' : 'updateOnEnter',
+		'blur .edit' : 'close'
+	},
+	initialize : function() {
+		this.listenTo(this.model, 'change', this.render);
+
+	},
+
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		this.$input = this.$('.edit');
+		return this;
+	},
+
+	edit : function() {
+		this.$el.addClass('editing');
+		this.$input.focus();
+	},
+
+	close : function() {
+		var value = this.$input.val().trim();
+		if (value) {
+			this.model.save({
+				title : value
+			});
+		}
+		this.$el.removeClass('editing');
+	},
+
+	updateOnEnter : function(e) {
+		if (e.which === ENTER_KEY) {
+			this.close();
+		}
+	}
+
+});
